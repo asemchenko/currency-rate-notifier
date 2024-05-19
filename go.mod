8cbfc1fa5ae8f4de9c50773eb942d31c4c425728
Andrii Semchenko <a7chenko@gmail.com>
2024-05-19 19:51:04 +0300

    Add basic DB migrations

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module currency-notifier
 go 1.22
 
 require (
+	github.com/bojanz/currency v1.2.3
+	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/gorilla/mux v1.8.0
 	github.com/swaggo/http-swagger v1.3.4
 	github.com/swaggo/swag v1.16.3
@@ -10,23 +12,19 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/bojanz/currency v1.2.3 // indirect
 	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
-	github.com/urfave/cli/v2 v2.3.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/tools v0.21.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"currency-notifier/internal/controller"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
 
 	_ "currency-notifier/docs"
 	httpSwagger "github.com/swaggo/http-swagger"
+
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
+
+var db *sql.DB
 
 // @title UAH currency application
 // @version 1.0
@@ -21,6 +31,8 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	initDb()
+
 	// Маршруты для контроллеров
 	r.HandleFunc("/api/rate", controller.GetRate).Methods("GET")
 	r.HandleFunc("/api/subscribe", controller.Subscribe).Methods("POST")
@@ -29,4 +41,57 @@ func main() {
 
 	log.Println("Server started at :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func initDb() {
+	// If no env variables - assume it's 'local' environment and use default values
+
+	dbHost := getEnv("DB_HOST", "localhost")
+	dbPort := getEnv("DB_PORT", "5432")
+	dbUser := getEnv("DB_USER", "postgres")
+	dbPassword := getEnv("DB_PASSWORD", "postgres")
+	dbName := getEnv("DB_NAME", "currency_notifier")
+	dbSSLMode := getEnv("DB_SSLMODE", "disable")
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		dbUser, dbPassword, dbHost, dbPort, dbName, dbSSLMode)
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = runMigrations(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+func runMigrations(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("could not create database driver: %w", err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://db/migrations",
+		"postgres", driver)
+	if err != nil {
+		return fmt.Errorf("could not start migration: %w", err)
+	}
+	err = m.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("could not run up migrations: %w", err)
+	}
+	log.Println("Migrations ran successfully")
+	return nil
 }
