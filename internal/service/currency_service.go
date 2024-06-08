6d6b6b0b2eadb650dcefbc47ab59b28779b9fd97
Andrii Semchenko <a7chenko@gmail.com>
2024-06-08 14:57:16 +0300

    Fix struct size

--- internal/service/currency_service.go
+++ internal/service/currency_service.go
@@ -29,11 +29,9 @@ var usdCode = getCurrencyCode("USD")
 var uahCode = getCurrencyCode("UAH")
 
 type CurrencyService struct {
-	// Dependencies
-	repo *repository.ExchangeRateRepository
-	// Cache variables
-	cacheRate  float64
 	cacheTime  time.Time
+	repo       *repository.ExchangeRateRepository
+	cacheRate  float64
 	cacheMutex sync.Mutex
 }
 
@@ -42,43 +40,33 @@ func NewCurrencyService(repo *repository.ExchangeRateRepository) *CurrencyServic
 		repo: repo,
 	}
 }
-
 func (s *CurrencyService) Init() error {
 	return s.ReloadRate()
 }
-
 func (s *CurrencyService) ReloadRate() error {
 	_, err := s.reloadRate()
-
 	return err
 }
-
 func (s *CurrencyService) reloadRate() (float64, error) {
 	rate, err := fetchRateFromAPI()
 	if err != nil {
 		return 0, err
 	}
 	log.Printf("USD to UAH rate: %f", rate)
-
 	s.updateCache(rate)
 	err = s.repo.SaveRate(rate)
 	if err != nil {
 		return 0, err
 	}
-
 	log.Printf("USD to UAH rate saved to the database")
-
 	return rate, nil
 }
-
 func (s *CurrencyService) GetUSDtoUAHRate() (float64, error) {
 	if rate, ok := s.getCachedRate(); ok {
 		return rate, nil
 	}
-
 	return s.reloadRate()
 }
-
 func getCurrencyCode(currencyCode string) int {
 	code, ok := currency.GetNumericCode(currencyCode)
 	if !ok {
@@ -88,49 +76,39 @@ func getCurrencyCode(currencyCode string) int {
 	if err != nil {
 		log.Fatal("Currency code is not a number")
 	}
-
 	return numericCode
 }
-
 func (s *CurrencyService) getCachedRate() (float64, bool) {
 	s.cacheMutex.Lock()
 	defer s.cacheMutex.Unlock()
-
 	if time.Since(s.cacheTime) < time.Hour {
 		return s.cacheRate, true
 	}
 	return 0, false
 }
-
 func fetchRateFromAPI() (float64, error) {
 	resp, err := http.Get(monobankAPI)
 	if err != nil {
 		return 0, err
 	}
 	defer resp.Body.Close()
-
 	if resp.StatusCode != http.StatusOK {
 		return 0, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
-
 	var rates []CurrencyRate
 	if err := json.NewDecoder(resp.Body).Decode(&rates); err != nil {
 		return 0, err
 	}
-
 	for _, rate := range rates {
 		if rate.CurrencyCodeA == usdCode && rate.CurrencyCodeB == uahCode {
 			return rate.RateSell, nil
 		}
 	}
-
 	return 0, errors.New("USD to UAH rate not found")
 }
-
 func (s *CurrencyService) updateCache(rate float64) {
 	s.cacheMutex.Lock()
 	defer s.cacheMutex.Unlock()
-
 	s.cacheRate = rate
 	s.cacheTime = time.Now()
 }
