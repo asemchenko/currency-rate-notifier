5d2a78c0ef83b3906393ba6c3694aa0465026317
Andrii Semchenko <a7chenko@gmail.com>
2024-06-08 15:43:39 +0300

    Fix subscription.go

--- internal/models/subscription.go
+++ internal/models/subscription.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Subscription struct {
-	ID           int       `json:"id" db:"id"`
-	Email        string    `json:"email" db:"email"`
 	SubscribedAt time.Time `json:"subscribed_at" db:"subscribed_at"`
+	Email        string    `json:"email" db:"email"`
+	ID           int       `json:"id" db:"id"`
 }
